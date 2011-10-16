38b8cd6d90f6608bde5884038e994c57cacd4f2d
Harold Paulson <haroldp@internal.org>
2011-10-15 19:06:37 -0700

    start web app

---
+++ index.php
@@ -0,0 +1,8 @@
+<?
+require_once('lib/init.php');
+require_once('lib/template.php');
+#require_once('contrib/tropo/tropo.class.php');
+#require_once('contrib/simplepie/simplepie.php');
+
+echo('Hello World');
+?>
