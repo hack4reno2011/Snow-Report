42e8a61e6bc7bbf16630562b9b28ce6173c836aa
Harold Paulson <haroldp@internal.org>
2011-10-15 19:51:00 -0700

    get tempplates started

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <?
 require_once('lib/init.php');
 require_once('lib/template.php');
-#require_once('contrib/tropo/tropo.class.php');
-#require_once('contrib/simplepie/simplepie.php');
+require_once('contrib/tropo/tropo.class.php');
+require_once('contrib/simplepie/simplepie.php');
 
-echo('Hello World');
+if ( $_REQUEST['f'] == 'voice' ) {
+	# tropo
+	$format = 'voice';
+}
+elseif ( $_REQUEST['f'] == 'rss' ) {
+	# tropo
+	$format = 'rss';
+}
+elseif ( $_REQUEST['f'] == 'html' ) {
+	# tropo
+	$format = 'html';
+}
+else {
+	# tropo
+	$format = 'html';
+}
+
+if ($type == 'html') {
+	$Body = new Template('templates/home.php');
+	
+	$T = new Template('templates/default.php');
+	$T->set('title', 'SnowLine');
+	$T->set('tagline', 'A SnowRSS Aggregator');
+	$T->set('body', $body);
+}
+
 ?>
--- templates/home.php
+++ templates/home.php
@@ -1,1 +1,6 @@
-Hello World!
+<p>
+Welcome to <strong>SnoLine</strong>!  <strong>SnoLine</strong> aggregates snow conditions from different ski resorts.
+</p>
+
+<h2>Resorts</h2>
+
